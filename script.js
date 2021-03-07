f470d1c4af2857ff8c28c695b3ad3915b5473599
Mark Alex Xbox <65962153+Blue-Sea2@users.noreply.github.com>
2021-03-07 09:06:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-var content = document.getElementById("pictures");
-function showPictures() {
-	if (content.style.display === "block") {
-		content.style.display = "none";
-	} else {
-		content.style.display = "block";
-	}
+function buttonClicked() {
+	var rotated = false;
+    	var div = document.getElementById('div'),
+        deg = rotated ? 0 : 66;
+
+	div.style.webkitTransform = 'rotate('+deg+'deg)'; 
+	div.style.mozTransform    = 'rotate('+deg+'deg)'; 
+	div.style.msTransform     = 'rotate('+deg+'deg)'; 
+	div.style.oTransform      = 'rotate('+deg+'deg)'; 
+	div.style.transform       = 'rotate('+deg+'deg)'; 
+
+	rotated = !rotated;
 }
